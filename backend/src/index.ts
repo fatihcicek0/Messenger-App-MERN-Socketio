b437de1efc7f19aa9a772f55cbeeaaec59668e77
Fatih <fatihcicek896@gmail.com>
2024-01-29 22:11:50 +0300

    add mongoose models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(3000,()=>{
+    console.log("listenin on localhost:3000 :) ")
+})
